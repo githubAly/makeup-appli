1d3c90eb4635d2c1a233f93b6728130896514723
Alyssa <alychrf62300@gmail.com>
2024-02-20 16:20:56 +0100

    ajout nouveaux fichiers

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, ScrollView, Dimensions } from 'react-native';
+import { Text } from 'react-native-paper';
+
+var width = Dimensions.get('window').width;
+export default function Header() {
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            <Text style={styles.titre}variant="displayMedium">Spacecraft</Text>
+        </ScrollView>
+      
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      paddingTop: 50,
+      paddingBottom: 30,
+      width: width,
+      backgroundColor: 'purple',
+    },
+
+})
